aa30e971686fccc5e8e1b06ce031f28a6d371344
Kyle Tucker <kyleatucker@gmail.com>
2014-12-05 11:42:20 -0500

    Finish deploy script Slack integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,30 @@
 # -- Load extensions
 require 'mina/git'
-# require 'slack-notifier' # <-- Uncomment this to use Slack notifications
 
 # -- Disable pretty-print to prevent known deploy issues
 set :term_mode, :exec
 
 # -- Repository settings
 set :repo_name,  'asmbs/thunderpress' # <-- Replace this with your repo identifier
-set :repo_url,   %[https://github.com/#{repo_name}.git]
-set :ref_tag,    '' # For tag-based deploys
-set :ref_commit, '' # For commit-based deploys
-set :ref_branch, '' # For branch-based deploys
+set :repo_url, %[https://github.com/#{repo_name}]
+set :repository, %[#{repo_url}.git]
+
+# -- Checkout references
+set :ref_tag,    ''       # For tag-based deploys
+set :ref_commit, ''       # For commit-based deploys
+set :ref_branch, 'master' # For branch-based deploys
 
 # -- Global SSH settings
 set :forward_agent, true
 
 # -- Shared paths and files
 set :shared_paths, []
+
+# -- Slack settings
+set :slack_enabled, false
+# set :slack_url, ''
+# set :slack_username, ''
+# set :slack_channel, ''
 
 
 # ----------------------------------------------------------------------------------------------
@@ -26,17 +34,17 @@ set :shared_paths, []
 task :environment do
 
   # -- Get server from command-line argument, or default to production
-  server = ENV.has_key?('server') ? ENV['server'] : 'production'
+  $server = ENV.has_key?('server') ? ENV['server'] : 'production'
 
   # -- Generate deploy settings for each server
-  case server
+  case $server
   when 'production'
 
     # -- SSH settings
     set :domain, ''
     set :user,   ''
     set :port,   '22'
-    # set :identity_file, '' # <-- If you're using a keypair, place the path to your private key here
+    # set :identity_file, '~/.ssh/path/to/key' # <-- If you're using a keypair, place the path to your private key here
 
     # -- Deploy path on server
     set :deploy_to, ''
@@ -45,11 +53,11 @@ task :environment do
     set_ref('tag')
 
     # -- Set lists of commands to run on success or failure of deployment
-    launch_commands = [
+    $launch_commands = [
       %[composer install],
       %[sudo service httpd restart]
     ]
-    clean_commands = []
+    $clean_commands = []
 
   end
 end
@@ -86,23 +94,21 @@ task :deploy => :environment do
     # -- Run `launch` subtask on successful deployment
     to :launch do
       # Run any set launch commands
-      if launch_commands.defined? && !launch_commands.empty?
-        launch.commands.each { |cmd| queue! cmd }
+      if defined?($launch_commands) && !$launch_commands.empty?
+        $launch_commands.each { |cmd| queue! %[#{cmd}] }
       end
-
-      # Post a Slack notification (maybe)
-      if slack_enabled?
-        # Do stuff
-      end
+      ref = commit ? commit : branch
+      slack_notify %[Deploy successful!\\n*Project:* #{repo_name} @ <#{repo_url}/tree/#{ref}|#{ref}>\\n*Server:* #{$server}]
     end
 
     # -- Run `clean` subtask on failed deployment
     to :clean do
-      if clean_commands.defined? && !clean_commands.empty?
-        clean_commands.each { |cmd| queue! cmd }
+      if defined?($clean_commands) && !$clean_commands.empty?
+        $clean_commands.each { |cmd| queue! cmd }
       end
+      slack_notify %[Deploy FAILED\\n*Project:* #{repo_name}\\n*Server:* #{$server}]
     end
-    
+
   end
 end
 
@@ -124,10 +130,29 @@ def set_ref (default_mode='branch')
   end
 end
 
-# -- Check to see whether the slack-notifier gem is enabled
+# -- Check to see whether Slack can be used
 def slack_enabled?
-  ref = Required::Module::const_get 'Slack'
-  return true
-rescue NameError
-  return false
+  return settings.slack_enabled && settings.slack_url
 end
+
+# -- Send Slack notification
+def slack_notify (message)
+  # Bail if no Slack post URL is defined
+  if !slack_enabled?
+    return
+  end
+  # Build payload
+  payload = '{'
+  payload << %["text":"#{message}"]
+  if settings.slack_username
+    payload << %[,"username":"#{settings.slack_username}"]
+  end
+  if settings.slack_channel
+    payload << %[,"channel":"#{settings.slack_channel}"]
+  end
+  payload << '}'
+  # Queue the POST command
+  queue %[curl -X POST --data-urlencode 'payload=#{payload}' #{settings.slack_url}]
+end
+
+
